4ad8fa02ef0547d63ecc8e4f77c62e065091c7d6
zoya <zoya.v@ya.ru>
2022-06-28 17:11:24 +0800

    move to h5py format

--- pogema-appo/offline_data_generator.py
+++ pogema-appo/offline_data_generator.py
@@ -7,6 +7,8 @@ from pogema import GridConfig
 import h5py
 from pomapf.wrappers import MatrixObservationWrapper
 import numpy as np
+import datetime
+from multiprocessing import Pool
 
 def drop_global_information(observations):
     for agent_idx, obs in enumerate(observations):
@@ -16,92 +18,88 @@ def drop_global_information(observations):
     return observations
 
 
-def example():
-    num_agents = 2
-    gc = GridConfig(seed=None, num_agents=num_agents, max_episode_steps=64, obs_radius=5, size=32, density=0.3)
 
-    # turn off with_animation to speedup generation speed
+def example(ind = 0, saver = 'h5py'):
+    gc = GridConfig(seed=None, num_agents=1, max_episode_steps=64, obs_radius=5, size=16, density=0.3)
     env = gym.make('POMAPF-v0', grid_config=gc, with_animations=True, auto_reset=False, egocentric_idx=None,
                    observation_type='MAPF')
-
-    agent_expirience = {'observations': [[]]*num_agents, 'next_observations': [[]]*num_agents,
-                        'actions': [[]]*num_agents, 'rewards': [[]]*num_agents,
-                        'terminals': [[]]*num_agents}
-
-    # fully centralized planning approach
     algo = Prioritized(PrioritizedConfig())
-    # partially observable decentralized planning approach
-    # algo = RePlan(RePlanConfig())
-
-    for _ in range(100000):
-        observations = env.reset()
-        algo.after_reset()
-        dones = [False, ...]
-
-        obs = MatrixObservationWrapper.to_matrix(observations)
-
-      #  print(obs.shape)
-      #  break
-
-       # print(agent_expirience['observations'])
-        while not all(dones):
-            for i in range(num_agents):
-               # print(obs[0]['obs'])
-                obs_ = obs[i]['obs']
-                #  print(obs.shape)
-                obs_ = np.asarray(obs_)
-                obs_ = obs_.reshape(-1, )
-                agent_expirience['observations'][i].append(obs_)
-
-            action = algo.act(observations, None, dones)
-           # print(action)
-            observations, rewards, dones, info = env.step(action)
-           # obs = drop_global_information(observations.copy())
+    start_point = 0
+    agent_expirience = {'observations': [], 'next_observations': [], 'actions': [], 'rewards': [], 'terminals': []}
+    with h5py.File('../examples/data_v2%d.hdf5'%ind, 'w') as f:
+        observations_ = f.create_dataset('observations', (100,363), maxshape=(500000000,363))
+        next_observations_ = f.create_dataset('next_observations', (100,363), maxshape=(500000000,363))
+        actions_ = f.create_dataset('actions', (100,1), maxshape=(500000000,1))
+        rewards_ = f.create_dataset('rewards', (100,), maxshape=(500000000,))
+        terminals_ = f.create_dataset('terminals', (100,), maxshape=(50000000,))
+        data_count = 500000
+        log_interval = 1000
+        for k in range(data_count):
+            if k%log_interval == 0:
+                print(f"{k}/{data_count}")
+            observations = env.reset()
+            algo.after_reset()
+            dones = [False, ...]
             obs = MatrixObservationWrapper.to_matrix(observations)
-            for i in range(num_agents):
-               # obs = MatrixObservationWrapper.to_matrix(observations)
-                obs_ = obs[i]['obs']
-                obs_ = np.asarray(obs_)
-                obs_ = obs_.reshape(-1, )
-             #   print(action[i])
-                agent_expirience['next_observations'][i].append(obs_)
-                act = 0 if action[i] is None else action[i]
-                agent_expirience['actions'][i].append((act)/(4))
-                agent_expirience['rewards'][i].append(rewards[i])
-                agent_expirience['terminals'][i].append(dones[i])
+            obs = obs[0]['obs']
+            obs = np.asarray(obs)
+            obs = obs.reshape(-1, )
+            while not all(dones):
+                agent_expirience['observations'].append(obs)
+                action = algo.act(observations, None, dones)
+                observations, rewards, dones, info = env.step(action)
+                obs = MatrixObservationWrapper.to_matrix(observations)
+                obs = obs[0]['obs']
+                obs = np.asarray(obs)
+                obs = obs.reshape(-1, )
+                agent_expirience['next_observations'].append(obs)
+                agent_expirience['actions'].append([(action[0])/(4)])
+                agent_expirience['rewards'].append(rewards[0])
+                agent_expirience['terminals'].append(dones[0])
+                algo.after_step(dones)
 
-            algo.after_step(dones)
+            add_data = len(agent_expirience['rewards'])
+          #  print()
+            observations_.resize((start_point+add_data,363))
+            next_observations_.resize((start_point+add_data,363))
+            actions_.resize((start_point+add_data,1))
+            rewards_.resize((start_point+add_data,))
+            terminals_.resize((start_point+add_data,))
 
-  #  print(agent_expirience['actions'])
-  #  print(agent_expirience['terminals'][-1])
-    data = {'observations': [] , 'next_observations': [] ,
-                        'actions': [] , 'rewards': [] ,
-                        'terminals': [] }
-    for i in range(num_agents):
-        data['observations'] += agent_expirience['observations'][i]
-        data['next_observations'] += agent_expirience['next_observations'][i]
-        data['actions'] += agent_expirience['actions'][i]
-        data['rewards'] += agent_expirience['rewards'][i]
-        data['terminals'] += agent_expirience['terminals'][i]
+            observations_[start_point:start_point+add_data] = agent_expirience['observations']
+            next_observations_[start_point:start_point+add_data] = agent_expirience['next_observations']
+            actions_[start_point:start_point+add_data] = agent_expirience['actions']
+            rewards_[start_point:start_point+add_data] = agent_expirience['rewards']
+            terminals_[start_point:start_point+add_data] = agent_expirience['terminals']
 
-    import pickle
+            agent_expirience = {'observations': [], 'next_observations': [], 'actions': [], 'rewards': [],
+                                'terminals': []}
+            start_point = add_data+start_point
 
 
-    for key in agent_expirience:
-        data[key] = np.asarray(data[key])
-  #  agent_expirience['next_observations'] = agent_expirience['next_observations'].reshape(-1,1)
-   # agent_expirience['observations'] = agent_expirience['observations'].reshape(-1, 1)
-    data['actions'] = data['actions'].reshape(-1, 1)
-    with open('../examples/data.pickle', 'wb') as f:
-        pickle.dump(data, f)
 
-    with h5py.File('../examples/data.hdf5', 'w') as f:
-        f.create_dataset('next_observations', data=data['next_observations'])
-        f.create_dataset('actions', data= data['actions'])
-        f.create_dataset('rewards', data=data['rewards'])
-        f.create_dataset('terminals', data=data['terminals'])
-        f.create_dataset('observations', data=data['observations'])
+    if saver == "pickle":
+        import pickle
+        for key in agent_expirience:
+            agent_expirience[key] = np.asarray(agent_expirience[key])
+        agent_expirience['actions'] = agent_expirience['actions'].reshape(-1, 1)
+        with open('../examples/data.pickle', 'wb') as f:
+            pickle.dump(agent_expirience, f)
+    else:
+        pass
+
+    # with h5py.File(f'../examples/data{ind}.hdf5', 'w') as f:
+    #     f.create_dataset('next_observations', data=agent_expirience['next_observations'])
+    #     f.create_dataset('actions', data= agent_expirience['actions'])
+    #     f.create_dataset('rewards', data=agent_expirience['rewards'])
+    #     f.create_dataset('terminals', data=agent_expirience['terminals'])
+    #     f.create_dataset('observations', data=agent_expirience['observations'])
 
 
 if __name__ == '__main__':
-    example()
+    # p = Pool(2)
+     a = datetime.datetime.now()
+  #   with p:
+     example()
+     b = datetime.datetime.now()
+   #  print(b-a)
